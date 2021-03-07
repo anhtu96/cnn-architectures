6a0614733ca2e036da0d2b751652465a18f0b0ea
Tu Ngo <tungo.ee.96@gmail.com>
2021-03-07 10:44:32 +0700

    convert VGG to PyTorch

--- models/vgg.py
+++ models/vgg.py
@@ -1,91 +1,59 @@
-#%%
-import tensorflow as tf
-from tensorflow import keras
+import torch
+import torch.nn as nn
 
+# Dictionary contains output channels for feature extractor, 'M' denotes Max-pooling layers.
+CONV_CHANNELS_DICT = {
+    11: [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512],
+    13: [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    16: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    19: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+}
+def make_layers(n_layers, has_BN=False) -> nn.Sequential:
+    """ Create feature extraction layers for VGG given VGG configuration (11/13/16/19 layers and with/without BatchNorm).
+    Args:
+        - n_layers: number of VGG layers (int).
+        - has_BN: create layers with or without BatchNorm (default: False).
+    Returns:
+        - a Sequential contains feature extraction layers.
+    """
+    layers = []
+    in_channels = 3
+    for out_channels in CONV_CHANNELS_DICT[n_layers]:
+        if out_channels == 'M':
+            layers.append(nn.MaxPool2d(2, stride=2))
+        else:
+            layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1))
+            if has_BN:
+                layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU(True))
+            in_channels = out_channels
+    layers = nn.Sequential(*layers)
+    return layers
 
+class VGG(nn.Module):
+    def __init__(self, n_layers=16, has_BN=False, n_classes=1000):
+        """ VGG class constructor.
+        Args:
+            - n_layers: number of layers (11/13/16/19).
+            - has_BN: create Conv layers with/without BatchNorm (default: False).
+            - n_classes: number of output classes.
+        """
+        super(VGG, self).__init__()
+        self.features = make_layers(n_layers, has_BN)
+        self.avgpool = nn.AdaptiveAvgPool2d(7)
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, n_classes),
+        )
 
-class VGG16(keras.Model):
-    def __init__(self, name='vgg16', dropout_rate=0.5):
-        super(VGG16, self).__init__(name=name)
-        fc_regularizer = keras.regularizers.L2(5e-4)
-        self.blocks = {
-            'conv1': keras.layers.Conv2D(filters=64, kernel_size=3, strides=(1,1), padding='same', name='conv1'),
-            'conv2': keras.layers.Conv2D(filters=64, kernel_size=3, strides=(1,1), padding='same', name='conv2'),
-            'maxpool1': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool1'),
-
-            'conv3': keras.layers.Conv2D(filters=128, kernel_size=3, strides=(1,1), padding='same', name='conv3'),
-            'conv4': keras.layers.Conv2D(filters=128, kernel_size=3, strides=(1,1), padding='same', name='conv4'),
-            'maxpool2': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool2'),
-            
-            'conv5': keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1,1), padding='same', name='conv5'),
-            'conv6': keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1,1), padding='same', name='conv6'),
-            'conv7': keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1,1), padding='same', name='conv7'),
-            'maxpool3': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool3'),
-
-            'conv8': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv8'),
-            'conv9': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv9'),
-            'conv10': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv10'),
-            'maxpool4': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool4'),
-
-            'conv11': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv11'),
-            'conv12': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv12'),
-            'conv13': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv13'),
-            'maxpool5': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool5'),
-
-            'flatten': keras.layers.Flatten(name='flatten'),
-            'fc14': keras.layers.Dense(units=4096, activation='relu', kernel_regularizer=fc_regularizer, name='fc14'),
-            'dropout1': keras.layers.Dropout(dropout_rate, name='dropout1'),
-            'fc15': keras.layers.Dense(units=4096, activation='relu', kernel_regularizer=fc_regularizer, name='fc15'),
-            'dropout2': keras.layers.Dropout(dropout_rate, name='dropout2'),
-            'fc16': keras.layers.Dense(units=1000, activation='relu', kernel_regularizer=fc_regularizer, name='fc16'),
-        }
-
-    def call(self, x):
-        for block in self.blocks:
-            x = self.blocks[block](x)
-        return x
-    
-
-class VGG19(keras.Model):
-    def __init__(self, name='vgg19', dropout_rate=0.5):
-        super(VGG19, self).__init__(name=name)
-        fc_regularizer = keras.regularizers.L2(5e-4)
-        self.blocks = {
-            'conv1': keras.layers.Conv2D(filters=64, kernel_size=3, strides=(1,1), padding='same', name='conv1'),
-            'conv2': keras.layers.Conv2D(filters=64, kernel_size=3, strides=(1,1), padding='same', name='conv2'),
-            'maxpool1': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool1'),
-
-            'conv3': keras.layers.Conv2D(filters=128, kernel_size=3, strides=(1,1), padding='same', name='conv3'),
-            'conv4': keras.layers.Conv2D(filters=128, kernel_size=3, strides=(1,1), padding='same', name='conv4'),
-            'maxpool2': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool2'),
-            
-            'conv5': keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1,1), padding='same', name='conv5'),
-            'conv6': keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1,1), padding='same', name='conv6'),
-            'conv7': keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1,1), padding='same', name='conv7'),
-            'conv8': keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1,1), padding='same', name='conv8'),
-            'maxpool3': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool3'),
-
-            'conv9': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv9'),
-            'conv10': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv10'),
-            'conv11': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv11'),
-            'conv12': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv12'),
-            'maxpool4': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool4'),
-
-            'conv13': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv13'),
-            'conv14': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv14'),
-            'conv15': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv15'),
-            'conv16': keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1,1), padding='same', name='conv16'),
-            'maxpool5': keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name='maxpool5'),
-
-            'flatten': keras.layers.Flatten(name='flatten'),
-            'fc17': keras.layers.Dense(units=4096, activation='relu', kernel_regularizer=fc_regularizer, name='fc17'),
-            'dropout1': keras.layers.Dropout(dropout_rate, name='dropout1'),
-            'fc18': keras.layers.Dense(units=4096, activation='relu', kernel_regularizer=fc_regularizer, name='fc18'),
-            'dropout2': keras.layers.Dropout(dropout_rate, name='dropout2'),
-            'fc19': keras.layers.Dense(units=1000, activation='relu', kernel_regularizer=fc_regularizer, name='fc19'),
-        }
-
-    def call(self, x):
-        for block in self.blocks:
-            x = self.blocks[block](x)
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
         return x
